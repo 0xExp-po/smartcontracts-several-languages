d11d73b3154a937f77de6d6fcee57b75990fd231
lucas-po <lucaschang1127@gmail.com>
2023-01-05 17:58:53 +0100

    Update crowdfunding.ts

--- crowdfunding/cairo 0.10.1/test/crowdfunding.ts
+++ crowdfunding/cairo 0.10.1/test/crowdfunding.ts
@@ -1,101 +1,131 @@
 import { expect } from "chai";
 import { starknet } from "hardhat";
-import { StarknetContract, StarknetContractFactory } from "hardhat/types/runtime";
+import {
+  StarknetContract,
+  StarknetContractFactory,
+} from "hardhat/types/runtime";
 import { TIMEOUT } from "./constants";
 import { ensureEnvVar } from "./util";
 
 describe("Starknet", function () {
-    this.timeout(TIMEOUT);
+  this.timeout(TIMEOUT);
 
-    let contractFactory: StarknetContractFactory;
-    let contract: StarknetContract;
-    let account: any;
-    let now: number;
-    let goal: number;
-    let days: number;
+  let contractFactory: StarknetContractFactory;
+  let contract: StarknetContract;
+  let account: any;
+  let now: number;
+  let goal: number;
+  let days: number;
 
-    before(async function () {
-        const accountAddress = ensureEnvVar("OZ_ACCOUNT_ADDRESS");
-        const accountPrivateKey = ensureEnvVar("OZ_ACCOUNT_PRIVATE_KEY");
-        account = await starknet.getAccountFromAddress(
-            accountAddress,
-            accountPrivateKey,
-            "OpenZeppelin"
-        );
-        console.log(`Account address: ${account.address}, public key: ${account.publicKey})`);
-        contractFactory = await starknet.getContractFactory(
-            "crowdfunding"
-        );
-        
-        console.log("Started deployment");
-        goal = 100;
-        days = 10;
-        const block = await starknet.getBlock();
-        const deployTime = block.timestamp;
-        now = deployTime + days * 24 * 60 * 60;
-        contract = await contractFactory.deploy({ initial_number_of_days: days, initial_goal: goal, initial_account_id: account.address });
-        console.log(`Deployed contract to ${contract.address} in tx ${contract.deployTxHash}`);
+  before(async function () {
+    const accountAddress = ensureEnvVar("OZ_ACCOUNT_ADDRESS");
+    const accountPrivateKey = ensureEnvVar("OZ_ACCOUNT_PRIVATE_KEY");
+    account = await starknet.getAccountFromAddress(
+      accountAddress,
+      accountPrivateKey,
+      "OpenZeppelin"
+    );
+    console.log(
+      `Account address: ${account.address}, public key: ${account.publicKey})`
+    );
+    contractFactory = await starknet.getContractFactory("crowdfunding");
 
-        await account.invoke(contract, "account_balance_increase", { amount: 1000, account_id: account.address });
+    console.log("Started deployment");
+    goal = 100;
+    days = 10;
+    const block = await starknet.getBlock();
+    const deployTime = block.timestamp;
+    now = deployTime + days * 24 * 60 * 60;
+    contract = await contractFactory.deploy({
+      initial_number_of_days: days,
+      initial_goal: goal,
+      initial_account_id: account.address,
     });
+    console.log(
+      `Deployed contract to ${contract.address} in tx ${contract.deployTxHash}`
+    );
 
-    it("should work for a fresh deployment", async function () {
-        const { res: deadline } = await contract.call("get_deadline");        
-        expect(deadline.toString()).to.deep.equal(now.toString());
-        
-        const { res: goal } = await contract.call("get_goal");
-        expect(goal).to.deep.equal(100n);
-
-        const { res: owner } = await contract.call("get_owner");
-        expect(owner).to.deep.equal(910652462498721021811173708055395672475122979568577554221771407653781759920n);
-
-        const { res: account_balance } = await contract.call("get_account_balance", {account_id: account.address});
-        expect(account_balance).to.deep.equal(1000n);
-
-        const { res: current_pledge } = await contract.call("get_current_pledge");
-        expect(current_pledge).to.deep.equal(0n)
+    await account.invoke(contract, "account_balance_increase", {
+      amount: 1000,
+      account_id: account.address,
     });
+  });
 
-    it("should work for a campaign refund", async function () {
-        await account.invoke(contract, "pledge", { amount: 1 });
+  it("should work for a fresh deployment", async function () {
+    const { res: deadline } = await contract.call("get_deadline");
+    expect(deadline.toString()).to.deep.equal(now.toString());
 
-        const { res: current_pledge } = await contract.call("get_current_pledge");
-        expect(current_pledge).to.deep.equal(1n);
+    const { res: goal } = await contract.call("get_goal");
+    expect(goal).to.deep.equal(100n);
 
-        const { res: account_balance } = await contract.call("get_account_balance", {account_id: account.address});
-        expect(account_balance).to.deep.equal(1000n-1n);
+    const { res: owner } = await contract.call("get_owner");
+    expect(owner).to.deep.equal(
+      910652462498721021811173708055395672475122979568577554221771407653781759920n
+    );
 
-        // TODO: fix time issue on devnet level
-        // const block = await starknet.getBlock();
-        // const deployTime = block.timestamp;
-        // await starknet.devnet.setTime(deployTime + (days + 1) * 24 * 60 * 60);
-
-        await account.invoke(contract, "get_full_refund");
-
-        const { res: account_balance_after_refund } = await contract.call("get_account_balance", {account_id: account.address});
-        expect(account_balance_after_refund).to.deep.equal(1000n);
+    const { res: accountBalance } = await contract.call("get_account_balance", {
+      account_id: account.address,
     });
+    expect(accountBalance).to.deep.equal(1000n);
 
-    it("should work for a campaign pledge", async function () {
-        await account.invoke(contract, "pledge", { amount: 101 });
-        
-        const { res: current_pledge } = await contract.call("get_current_pledge");
-        expect(current_pledge).to.deep.equal(101n);
+    const { res: currentPledge } = await contract.call("get_current_pledge");
+    expect(currentPledge).to.deep.equal(0n);
+  });
 
-        const { res: account_balance } = await contract.call("get_account_balance", {account_id: account.address});
-        expect(account_balance).to.deep.equal(1000n-101n);
+  it("should work for a campaign refund", async function () {
+    await account.invoke(contract, "pledge", { amount: 1 });
+
+    const { res: currentPledge } = await contract.call("get_current_pledge");
+    expect(currentPledge).to.deep.equal(1n);
+
+    const { res: accountBalance } = await contract.call("get_account_balance", {
+      account_id: account.address,
     });
+    expect(accountBalance).to.deep.equal(1000n - 1n);
 
-    it("should work for a campaign claim funds", async function () {
-        const { res: account_balance } = await contract.call("get_account_balance", {account_id: account.address});
-        expect(account_balance).to.deep.equal(899n);
+    // TODO: fix time issue on devnet level
+    // const block = await starknet.getBlock();
+    // const deployTime = block.timestamp;
+    // await starknet.devnet.setTime(deployTime + (days + 1) * 24 * 60 * 60);
 
-        await account.invoke(contract, "claim_funds");
+    await account.invoke(contract, "get_full_refund");
 
-        const { res: get_end_of_campaign_after_claim_funds } = await contract.call("get_end_of_campaign");
-        expect(get_end_of_campaign_after_claim_funds).to.deep.equal(1n);
+    const { res: accountBalanceAfterRefund } = await contract.call(
+      "get_account_balance",
+      { account_id: account.address }
+    );
+    expect(accountBalanceAfterRefund).to.deep.equal(1000n);
+  });
 
-        const { res: account_balance_after_claim_funds } = await contract.call("get_account_balance", {account_id: account.address});
-        expect(account_balance_after_claim_funds).to.deep.equal(1000n);
+  it("should work for a campaign pledge", async function () {
+    await account.invoke(contract, "pledge", { amount: 101 });
+
+    const { res: currentPledge } = await contract.call("get_current_pledge");
+    expect(currentPledge).to.deep.equal(101n);
+
+    const { res: accountBalance } = await contract.call("get_account_balance", {
+      account_id: account.address,
     });
+    expect(accountBalance).to.deep.equal(1000n - 101n);
+  });
+
+  it("should work for a campaign claim funds", async function () {
+    const { res: accountBalance } = await contract.call("get_account_balance", {
+      account_id: account.address,
+    });
+    expect(accountBalance).to.deep.equal(899n);
+
+    await account.invoke(contract, "claim_funds");
+
+    const { res: getEndOfCampaignAfterClaimFunds } = await contract.call(
+      "get_end_of_campaign"
+    );
+    expect(getEndOfCampaignAfterClaimFunds).to.deep.equal(1n);
+
+    const { res: accountBalanceAfterClaimFunds } = await contract.call(
+      "get_account_balance",
+      { account_id: account.address }
+    );
+    expect(accountBalanceAfterClaimFunds).to.deep.equal(1000n);
+  });
 });
