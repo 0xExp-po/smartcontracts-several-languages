74640b2347dae552f47d7113f5ade56632408ac8
lucas-po <lucaschang1127@gmail.com>
2023-02-18 02:27:27 +0100

    Add comments

--- crowdfunding/solidity 0.8.17/contracts/crowdfunding.sol
+++ crowdfunding/solidity 0.8.17/contracts/crowdfunding.sol
@@ -2,11 +2,19 @@
 pragma solidity ^0.8.17;
 
 contract Crowdfunding {
+    // The address of the project creator
     address public owner;
+    
+    // The project deadline (in seconds since the Unix epoch)
     uint256 public deadline;
+    
+    // The minimum goal for the project (in wei)
     uint256 public goal;
+    
+    // The mapping of funding that has been raised (in wei)
     mapping(address => uint256) public pledgeOf;
 
+    // Constructor function to initialize the contract
     constructor(uint256 _numberOfDays, uint256 _goal) {
         require(_numberOfDays > 0, "numberOfDays must be greater than zero");
         require(_goal > 0, "goal must be greater than zero");
@@ -16,12 +24,14 @@ contract Crowdfunding {
         goal = _goal;
     }
 
+    // Function to allow backers to contribute to the project
     function pledge() public payable {
         require(block.timestamp < deadline);
 
         pledgeOf[msg.sender] += msg.value;
     }
 
+    // Function to allow the project creator to retrieve the funds
     function claimFunds() public {
         require(address(this).balance >= goal);
         require(block.timestamp >= deadline);
@@ -30,6 +40,7 @@ contract Crowdfunding {
         payable(msg.sender).transfer(address(this).balance);
     }
 
+    // Function to allow backers to get funds if the goal will not be reached
     function getRefund() public {
         require(address(this).balance < goal);
         require(block.timestamp >= deadline);
