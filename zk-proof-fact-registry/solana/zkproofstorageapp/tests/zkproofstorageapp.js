689abc8bade01d87dccad527783528096c6581d9
lucas-po <lucaschang1127@gmail.com>
2023-04-04 14:04:32 +0200

    Hello World example

--- zk-proof-fact-registry/solana/zkproofstorageapp/tests/zkproofstorageapp.js
+++ zk-proof-fact-registry/solana/zkproofstorageapp/tests/zkproofstorageapp.js
@@ -1,13 +1,43 @@
-const anchor = require("@coral-xyz/anchor");
+const assert = require("assert");
+const anchor = require("@project-serum/anchor");
+const { SystemProgram } = anchor.web3;
 
 describe("zkproofstorageapp", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = new anchor.getProvider();
+  anchor.setProvider(provider);
+  const program = anchor.workspace.Zkproofstorageapp;
+  it("It initializes the account", async () => {
+    const baseAccount = anchor.web3.Keypair.generate();
+    await program.rpc.initialize("Hello World", {
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [baseAccount],
+    });
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const program = anchor.workspace.Zkproofstorageapp;
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('Data: ', account.data);
+    assert.ok(account.data === "Hello World");
+    _baseAccount = baseAccount;
+
+  });
+
+  it("Updates a previously created account", async () => {
+    const baseAccount = _baseAccount;
+
+    await program.rpc.update("Some new data", {
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+      },
+    });
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('Updated data: ', account.data)
+    assert.ok(account.data === "Some new data");
+    console.log('all account data:', account)
+    console.log('All data: ', account.dataList);
+    assert.ok(account.dataList.length === 2);
   });
 });
